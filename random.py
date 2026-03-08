23a32c82a6989e61d916dd122fac9c37bebe7c2d
jhargrove09 <j.hargrove09@gmail.com>
2026-03-08 01:25:18 -0500

    mainly working

--- random.py
+++ random.py
@@ -1,8 +1,26 @@
-#All the user to input how many EC2 instances they want names for and output the same amount of unique names.
-
-#Allow the user to input the name of their department that is used in the unique name.
-department = input('What department is will these instances be used for?\n')
-
-#Generate random characters and numbers that will be included in the unique name.
 import random
+import string
 
+def character_number(department_choice):
+    number = random.randint(0, 9)
+
+    if department_choice == "marketing":
+        letter = "M"
+    elif department_choice == "accounting":
+        letter = "A"
+    elif department_choice == "finops":
+        letter = "F"
+    else:
+        letter = random.choice(string.ascii_uppercase)
+
+    return f"{letter}{number}"
+
+def main():
+    instance_count = int(input('How many instances do you need names for?\n'))
+    department_choice = input('What department will these instances be used for?\n').lower()
+
+    print(f'Generating {instance_count} names for department: {department_choice}')
+    for i in range(instance_count):
+        print(f"Instance {i + 1}: {character_number(department_choice)}")
+
+main()
