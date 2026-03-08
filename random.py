846f33d3aa5668f78ec6ed296fdbbd750d10350e
jhargrove09 <j.hargrove09@gmail.com>
2026-03-07 22:03:06 -0500

    instructions
    instructions and beginning script process

---
+++ random.py
@@ -0,0 +1,8 @@
+#All the user to input how many EC2 instances they want names for and output the same amount of unique names.
+
+#Allow the user to input the name of their department that is used in the unique name.
+department = input('What department is will these instances be used for?\n')
+
+#Generate random characters and numbers that will be included in the unique name.
+import random
+
